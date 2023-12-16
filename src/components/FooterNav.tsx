1d4d891d340873458cb0a6bfa1b0ead788f9a7c8
alabo-excel <iamalaboexcel@gmail.com>
2023-12-16 12:39:55 +0100

    first commit

---
+++ src/components/FooterNav.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const FooterNav = () => {
+  return (
+    <footer>
+      hello footer
+    </footer>
+  );
+};
+
+export default FooterNav;
---
+++ src/components/HeaderNav.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const HeaderNav = () => {
+  return (
+    <header>
+      hello header
+    </header>
+  );
+};
+
+export default HeaderNav;
