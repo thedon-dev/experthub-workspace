203408fce7afc082e808a891c4d8ddd8da10689b
alabo-excel <iamalaboexcel@gmail.com>
2024-03-09 10:05:56 +0100

    striked fee commit

--- src/components/cards/CoursesCard.tsx
+++ src/components/cards/CoursesCard.tsx
@@ -60,11 +60,8 @@ const CoursesCard = ({ course, getCourse }: { course: CourseType, getCourse: () 
     <div className="p-2 w-full shadow-md my-3 rounded-md bg-white">
       <img className="rounded-md w-full h-44 object-cover" src={course.thumbnail} alt="" />
       <h3 className="font-medium my-3">{course.title}
-        {pathname.includes("courses") ? <Link href={`/admin/${course.type}?page=${course._id}&view=true`}>
-          <button className='bg-primary p-2 rounded-md'>
-            {course.type === 'online' ? 'Join Live' : course.type}</button>
-        </Link> : <button onClick={() => setOpen(true)} className='bg-primary p-2 rounded-md'>
-          {course.type === 'online' ? 'Join Live' : course.type}</button>}  </h3>
+        {pathname.includes("courses") ? <button onClick={() => setOpen(true)} className='bg-primary p-2 rounded-md'>
+          {course.type === 'online' ? 'Join Live' : course.type}</button> : null}  </h3>
       <p className='text-xs'>{course.about.substring(0, 50)}...</p>
 
       <div className='flex justify-between my-3'>
--- src/components/cards/RecommendedCard.tsx
+++ src/components/cards/RecommendedCard.tsx
@@ -8,12 +8,12 @@ const RecommendedCard = ({ course, call }: { course: CourseType, call: any }) =>
   return (
     <div className='flex justify-between border p-3 my-3 lg:w-[49%] rounded-md border-[#1E1E1E75]'>
       <div className='w-52'>
-        <img className='w-full h-20 object-cover rounded-md' src={course.thumbnail} alt="" />
+        <img className='w-full h-full object-cover rounded-md' src={course.thumbnail} alt="" />
       </div>
       <div className='mx-4 w-full'>
-        <p className='text-primary text-sm'>{course.category}. <span className='text-black'> {course.instructorName}</span></p>
-        <p className='font-medium'>{course.title}</p>
-        <p className='text-sm'>{course.startDate}</p>
+        <p className='text-primary text-sm'>{course.category}. <span className='text-black'> by {course.instructorName}</span></p>
+        <p className='font-medium text-base'>{course.title}</p>
+        <p className='text-sm'><span>₦ {course.fee - course.strikedFee}</span> <span className='line-through	text-gray'>{course.fee}</span></p>
       </div>
       <button onClick={() => setOpen(true)} className='p-2 w-52 bg-primary my-auto rounded-sm'>Enrol Now</button>
       <CourseDetails course={course} open={open} type='enroll' call={call} handleClick={() => setOpen(false)} />
