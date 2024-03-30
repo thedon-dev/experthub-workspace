45df1a2e0c0a5caf86840bc8b0a68d6be311154d
Konfil <controleverything007@gmail.com>
2024-03-30 15:02:43 +0100

    more commit

--- src/components/modals/Notification.jsx
+++ src/components/modals/Notification.jsx
@@ -102,7 +102,7 @@ export default function Notifications() {
             <button aria-describedby={id} className="text-[20px] shadow-md rounded-full px-3 relative" onClick={handleClick}>
                 <img src="/images/icons/notification.svg" className='w-[20px] h-[20px]' alt="" />
                 {
-                    notifications?.filter(data => !data.read).length > 0 && <div className='absolute text-white text-[11px] top-0.5 -right-0.5 flex items-center justify-center w-[15px] h-[15px] rounded-full bg-red-500'>{notifications?.filter(data => !data.read).length || 0}</div>
+                    notifications?.filter(data => !data.read).length > 0 && <div className='absolute text-white text-[11px] top-2 right-2 flex items-center justify-center w-[15px] h-[15px] rounded-full bg-red-500'>{notifications?.filter(data => !data.read).length || 0}</div>
                 }
 
             </button>
