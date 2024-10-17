40a7585be3b2bb8be7ab935290eb8fc09c110de5
evansgiayetor@gmail.com <igbinosasasu@gmail.com>
2024-10-17 11:48:15 +0100

    added more stuff to make it work

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 export default function Loader() {
     return (
-        <div className='flex fixed top-0 left-0 items-center z-[9999999] bg-[rgb(0,0,0,0.7)] justify-center h-screen w-full'>
+        <div className='flex fixed top-0 left-0 items-center z-[9999999] bg-white justify-center h-screen w-full'>
             <div className='inline-flex justify-center items-center flex-col gap-3'>
                 <span className="loader2"></span>
-                <span className="loader3">Authenticating</span>
+                <span className="loader3 text-black">Authenticating</span>
             </div>
 
         </div>
