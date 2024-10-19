7fb7652e1bea694afd33fcc42d656b894d8da15a
evansgiayetor@gmail.com <igbinosasasu@gmail.com>
2024-10-19 14:14:38 +0100

    more sthing s are being added

--- src/utils/apiService.js
+++ src/utils/apiService.js
@@ -5,7 +5,6 @@ const apiService = axios.create({
     baseURL: "https://seashell-app-nejbh.ondigitalocean.app/",
     // baseURL: "http://127.0.0.1:3001/",
 
-    // 
 })
 
 export default apiService;
