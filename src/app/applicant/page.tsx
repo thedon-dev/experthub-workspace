0cd0afc8adca435abba0b86002ba764cacf1b009
iamalaboexcel <iamalaboexcel@gmail.com>
2024-11-05 09:45:32 +0100

    update commit

--- src/app/applicant/page.tsx
+++ src/app/applicant/page.tsx
@@ -101,9 +101,10 @@ const applicant = () => {
             <div className='!flex w-full flex-wrap'>
               <p onClick={() => setActive("")} className='m-2 bg-gray cursor-pointer px-2 text-base'>All</p>
               <p onClick={() => setActive(user.assignedCourse)} className='m-2 bg-gray px-2 cursor-pointer text-base'>{user.assignedCourse}</p>
-              {user.otherCourse?.map((single: string, index: any) => <p key={index} onClick={() => setActive(single)} className='m-2 bg-gray cursor-pointer px-2 text-base'>{single}</p>)}
+              {user.otherCourse?.map((single: string, index: any) => single.length === 0 ? null : <p key={index} onClick={() => setActive(single)} className='m-2 bg-gray cursor-pointer px-2 text-base'>{single}</p>)}
             </div>
           </div>
+          
           <p onClick={() => setView(view === 3 ? courses.length : 3)} className='text-[#DC9F08] cursor-pointer mt-auto'>VIEW {view === 3 ? "ALL" : "LESS"}</p>
         </div>
         <div className='flex flex-wrap justify-between'>
