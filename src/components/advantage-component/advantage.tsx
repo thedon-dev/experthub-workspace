309c6a20ffd0f5cbaa10fa04e4730dbec3434538
Prince Lee <prince_lee.nna85@ymail.com>
2025-01-09 14:22:10 +0100

    reduce hero image size

--- src/components/advantage-component/advantage.tsx
+++ src/components/advantage-component/advantage.tsx
@@ -28,14 +28,19 @@ const Advantages = () => {
         </h2>
         <div className="grid grid-cols-1 md:grid-cols-3 gap-8 justify-items-center">
           {advantages.map((advantage) => (
-            <div key={advantage.id} className="flex flex-col items-start  md:items-center font-sans">
-              <div className="flex flex-row ">
-                <div className="flex items-center justify-center w-8 h-8 rounded-full bg-[#FEEDCD] text-primary text-lg font-bold mb-4 mr-4">
+            <div
+              key={advantage.id}
+              className="flex flex-col items-center md:items-start font-sans w-full md:w-auto"
+            >
+              <div className="flex flex-col md:flex-row items-center md:items-start">
+                <div className="flex items-center justify-center w-8 h-8 rounded-full bg-[#FEEDCD] text-primary text-lg font-bold mb-4 md:mb-0 md:mr-4">
                   {advantage.id}
                 </div>
-                <div className="w-96">
+                <div className="w-full md:w-96 text-center md:text-left">
                   <h3 className="text-xl font-bold mb-2">{advantage.title}</h3>
-                  <p className="text-base text-gray-600">{advantage.description}</p>
+                  <p className="text-base text-gray-600">
+                    {advantage.description}
+                  </p>
                 </div>
               </div>
             </div>
