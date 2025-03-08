d8d885b1ef70c3ad5f47c9ca6f5606d9afbff142
Victor Jack <67293106+thedon-dev@users.noreply.github.com>
2025-03-08 08:00:18 +0100

    god abeg

--- src/components/cards/EventCard.tsx
+++ src/components/cards/EventCard.tsx
@@ -1,132 +1,207 @@
-import { CourseType } from '@/types/CourseType';
-import React, { useState } from 'react';
-import CourseDetails from '../modals/CourseDetails';
-import { Dropdown, MenuProps, Progress, notification } from 'antd';
-import Share from '../Share';
-import Link from 'next/link';
-import { usePathname } from 'next/navigation';
-import AddEvents from '../modals/AddEvents';
-import ImageViewer from '../ImageViewer';
-import Participants from '../Participants';
-import apiService from '@/utils/apiService';
+import { CourseType } from "@/types/CourseType";
+import React, { useState } from "react";
+import CourseDetails from "../modals/CourseDetails";
+import { Dropdown, MenuProps, Progress, notification } from "antd";
+import Share from "../Share";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import AddEvents from "../modals/AddEvents";
+import ImageViewer from "../ImageViewer";
+import Participants from "../Participants";
+import apiService from "@/utils/apiService";
 
-const EventCard = ({ event, action }: { event: CourseType, action: any }) => {
-  const [open, setOpen] = useState(false)
-  const [edit, setEdit] = useState(false)
-  const [deletec, setDelete] = useState(false)
-  const [view, setView] = useState(false)
-  const pathname = usePathname()
+const EventCard = ({ event, action }: { event: CourseType; action: any }) => {
+  const [open, setOpen] = useState(false);
+  const [edit, setEdit] = useState(false);
+  const [deletec, setDelete] = useState(false);
+  const [view, setView] = useState(false);
+  const pathname = usePathname();
   const [api, contextHolder] = notification.useNotification();
 
-  const items: MenuProps['items'] = [
+  const items: MenuProps["items"] = [
     {
-      key: '1',
-      label: (
-        <p onClick={() => setEdit(true)} >Edit Event</p>
-      ),
+      key: "1",
+      label: <p onClick={() => setEdit(true)}>Edit Event</p>,
     },
     {
-      key: '2',
-      label: (
-        <p onClick={() => setDelete(true)}>Delete Event</p>
-      ),
+      key: "2",
+      label: <p onClick={() => setDelete(true)}>Delete Event</p>,
     },
     {
-      key: '3',
-      label: (
-        <p onClick={() => setView(true)}>View Participants</p>
-      ),
+      key: "3",
+      label: <p onClick={() => setView(true)}>View Participants</p>,
     },
     {
-      key: '4',
-      label: (
-        <Share type='event' course={event} />
-      ),
+      key: "4",
+      label: <Share type="event" course={event} />,
     },
   ];
 
   const deleteCourse = async () => {
-    apiService.delete(`/events/delete/${event._id}`)
-      .then(function (response) {
-        // getCourse()
-        action()
-        api.open({
-          message: 'Event Deleted Successfully!'
-        });
-        setDelete(false)
-        console.log(response)
-      })
-  }
+    apiService.delete(`/events/delete/${event._id}`).then(function (response) {
+      // getCourse()
+      action();
+      api.open({
+        message: "Event Deleted Successfully!",
+      });
+      setDelete(false);
+      console.log(response);
+    });
+  };
 
   function calculateProgress(enrolled: number, target: number) {
     return (enrolled / target) * 100;
   }
 
   return (
-    <div className='lg:w-[32%] my-3'>
+    <div className="lg:w-[32%] my-3">
       {contextHolder}
-      <div className='p-2 rounded-md bg-white'>
+      <div className="p-2 rounded-md bg-white">
         <ImageViewer image={event.thumbnail} />
         {/* <img className='rounded-md h-44 object-cover w-full' src={event.thumbnail} alt="" /> */}
       </div>
       <div>
         {/* <p className='text-primary'>Course by {event.author}</p> */}
-        <div className='flex flex-wrap'> <p className='text-xl font-medium'>{event.title}</p>
-          <Link href={pathname.includes("admin") ? `/admin/${event._id}?page=event` : `/tutor/${event._id}?page=event`}><button className='bg-primary ml-3 text-sm p-1 rounded-md'>{event.mode}</button></Link>
+        <div className="flex flex-wrap">
+          {" "}
+          <p className="text-xl font-medium">{event.title}</p>
+          <Link
+            href={
+              pathname.includes("admin")
+                ? `/admin/${event._id}?page=event`
+                : `/tutor/${event._id}?page=event`
+            }
+          >
+            <button className="bg-primary ml-3 text-sm p-1 rounded-md">
+              {event.mode}
+            </button>
+          </Link>
         </div>
-        <p className='text-sm'>{event.about.substring(0, 100)}</p>
-        <div className='flex justify-between my-3'>
+        <p className="text-sm">{event.about.substring(0, 100)}</p>
+        <div className="flex justify-between my-3">
           <div>
-            <p className='text-xs my-1'>Students {event.enrolledStudents.length}</p>
-            <div className='flex ml-1'>
-              {event.enrolledStudents.slice(0, 6).map(event => <img key={event._id} src={event.profilePicture  ? event.profilePicture : '/images/user.png'} className='w-5 rounded-full h-5 -ml-1' alt="" />)}
+            <p className="text-xs my-1">
+              Students {event.enrolledStudents.length}
+            </p>
+            <div className="flex ml-1">
+              {event.enrolledStudents.slice(0, 6).map((event) => (
+                <img
+                  key={event._id}
+                  src={
+                    event.profilePicture
+                      ? event.profilePicture
+                      : "/images/user.png"
+                  }
+                  className="w-5 rounded-full h-5 -ml-1"
+                  alt=""
+                />
+              ))}
               {/* <img src="/images/user.png" className='w-5 h-5' alt="" />
             <img src="/images/user.png" className='w-5 h-5 -ml-2' alt="" />
             <img src="/images/user.png" className='w-5 h-5 -ml-2' alt="" /> */}
             </div>
           </div>
-          <div className='w-[70%] '>
-            <div className='ml-auto text-right'>
+          <div className="w-[70%] ">
+            <div className="ml-auto text-right">
               <Dropdown menu={{ items }} trigger={["click"]}>
-                <button className='bg-transparent'>
-                  <img className='w-4 h-4' src="/images/icons/edit.svg" alt="" />
+                <button className="bg-transparent">
+                  <img
+                    className="w-4 h-4"
+                    src="/images/icons/edit.svg"
+                    alt=""
+                  />
                 </button>
               </Dropdown>
             </div>
-            <div className='flex my-auto'>
-              <p className='text-xs font-medium w-full'>Overall progress</p>
-              <Progress percent={parseInt(calculateProgress(event.enrolledStudents.length, event.target).toFixed())} size="small" />
+            <div className="flex my-auto">
+              <p className="text-xs font-medium w-full">Overall progress</p>
+              <Progress
+                percent={parseInt(
+                  calculateProgress(
+                    event.enrolledStudents.length,
+                    event.target
+                  ).toFixed()
+                )}
+                size="small"
+              />
             </div>
           </div>
         </div>
-        <div className='text-center my-3'>
-          {event.type === "online" ? <button onClick={() => setOpen(true)} className='bg-primary p-2 w-44 text-white'>Join Live </button> : <button onClick={() => setOpen(true)} className='border border-[#1E1E1E] text-[#DC9F08] p-2 w-44 mx-auto'>View Details</button>}
+        <div className="text-center my-3">
+          {event.type === "online" ? (
+            <button
+              onClick={() => setOpen(true)}
+              className="bg-primary p-2 w-44 text-white"
+            >
+              Join Live{" "}
+            </button>
+          ) : (
+            <button
+              onClick={() => setOpen(true)}
+              className="border border-[#1E1E1E] text-[#DC9F08] p-2 w-44 mx-auto"
+            >
+              View Details
+            </button>
+          )}
         </div>
       </div>
-      <CourseDetails course={event} open={open} call={null} action={"Event"} type='view' handleClick={() => setOpen(false)} />
-      <AddEvents open={edit} course={event} handleClick={() => setEdit(false)} />
-      {
-        deletec && <div>
-          <div onClick={() => setDelete(false)} className='fixed cursor-pointer bg-[#000000] opacity-50 top-0 left-0 right-0 w-full h-[100vh] z-10'></div>
-          <div className='fixed top-10 bottom-10 left-0 rounded-md right-0 lg:w-[30%] w-[90%] h-[50%] mx-auto z-20 bg-[#F8F7F4]'>
-            <div className='shadow-[0px_1px_2.799999952316284px_0px_#1E1E1E38] p-4 lg:px-12 flex justify-between'>
-              <p className='font-medium'></p>
-              <img onClick={() => setDelete(false)} className='w-6 h-6 cursor-pointer' src="/images/icons/material-symbols_cancel-outline.svg" alt="" />
+      <CourseDetails
+        workspace={event}
+        course={event}
+        open={open}
+        call={null}
+        action={"Event"}
+        type="view"
+        handleClick={() => setOpen(false)}
+      />
+      <AddEvents
+        open={edit}
+        course={event}
+        handleClick={() => setEdit(false)}
+      />
+      {deletec && (
+        <div>
+          <div
+            onClick={() => setDelete(false)}
+            className="fixed cursor-pointer bg-[#000000] opacity-50 top-0 left-0 right-0 w-full h-[100vh] z-10"
+          ></div>
+          <div className="fixed top-10 bottom-10 left-0 rounded-md right-0 lg:w-[30%] w-[90%] h-[50%] mx-auto z-20 bg-[#F8F7F4]">
+            <div className="shadow-[0px_1px_2.799999952316284px_0px_#1E1E1E38] p-4 lg:px-12 flex justify-between">
+              <p className="font-medium"></p>
+              <img
+                onClick={() => setDelete(false)}
+                className="w-6 h-6 cursor-pointer"
+                src="/images/icons/material-symbols_cancel-outline.svg"
+                alt=""
+              />
             </div>
-            <div className='lg:p-10 p-4 text-center'>
-              <h1 className='text-2xl'>Are you sure you want to delete this event?</h1>
+            <div className="lg:p-10 p-4 text-center">
+              <h1 className="text-2xl">
+                Are you sure you want to delete this event?
+              </h1>
               <div>
-                <div className='flex my-4 justify-center'>
-                  <button onClick={() => deleteCourse()} className='mx-4 bg-primary p-2 rounded-md'>Delete</button>
-                  <button onClick={() => setDelete(false)} className='mx-4'>Cancel</button>
+                <div className="flex my-4 justify-center">
+                  <button
+                    onClick={() => deleteCourse()}
+                    className="mx-4 bg-primary p-2 rounded-md"
+                  >
+                    Delete
+                  </button>
+                  <button onClick={() => setDelete(false)} className="mx-4">
+                    Cancel
+                  </button>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      }
-      <Participants view={view} event={event} hndelClick={() => setView(false)} type="Event" />
-
+      )}
+      <Participants
+        view={view}
+        event={event}
+        hndelClick={() => setView(false)}
+        type="Event"
+      />
     </div>
   );
 };
--- src/components/cards/RecommendedCard.tsx
+++ src/components/cards/RecommendedCard.tsx
@@ -1,57 +1,128 @@
-import { CourseType } from '@/types/CourseType';
-import React, { useState } from 'react';
-import CourseDetails from '../modals/CourseDetails';
-import ShareModal from '../modals/ShareModal';
-import AppointmentModal from '../modals/AppointmentModal';
+import { CourseType } from "@/types/CourseType";
+import React, { useState } from "react";
+import CourseDetails from "../modals/CourseDetails";
+import ShareModal from "../modals/ShareModal";
+import AppointmentModal from "../modals/AppointmentModal";
 
-const RecommendedCard = ({ course, call }: { course: CourseType, call: any }) => {
-  const [open, setOpen] = useState(false)
-  const [share, setShare] = useState(false)
-  const [appointment, setAppointment] = useState(false)
+const RecommendedCard = ({
+  course,
+  call,
+}: {
+  course: CourseType;
+  call: any;
+}) => {
+  const [open, setOpen] = useState(false);
+  const [share, setShare] = useState(false);
+  const [appointment, setAppointment] = useState(false);
 
   return (
-    <div className='lg:flex justify-between border p-3 my-3 lg:w-[49%] rounded-md border-[#1E1E1E75]'>
-      <div className='lg:w-52'>
-        {typeof course.thumbnail === 'string' ? <img className='w-full h-full object-cover rounded-md' src={course?.thumbnail} alt="" /> : course.thumbnail.type === 'image' ? <img className='w-full h-full object-cover rounded-md' src={course.thumbnail.url} alt="" /> :
+    <div className="lg:flex justify-between border p-3 my-3 lg:w-[49%] rounded-md border-[#1E1E1E75]">
+      <div className="lg:w-52">
+        {typeof course.thumbnail === "string" ? (
+          <img
+            className="w-full h-full object-cover rounded-md"
+            src={course?.thumbnail}
+            alt=""
+          />
+        ) : course.thumbnail.type === "image" ? (
+          <img
+            className="w-full h-full object-cover rounded-md"
+            src={course.thumbnail.url}
+            alt=""
+          />
+        ) : (
           <video
             src={course.thumbnail.url}
             width="100"
-            autoPlay muted
+            autoPlay
+            muted
             className="embed-responsive-item w-full object-cover h-full"
           >
             <source src={course.thumbnail.url} type="video/mp4" />
-          </video>}
+          </video>
+        )}
       </div>
-      <div className='lg:mx-4 sm:my-2 w-full'>
-        <p className='text-primary text-sm'>{course.category}. <span className='text-black'> by {course.instructorName}</span></p>
-        <p className='font-medium text-base'>{course.title}</p>
+      <div className="lg:mx-4 sm:my-2 w-full">
+        <p className="text-primary text-sm">
+          {course.category}.{" "}
+          <span className="text-black"> by {course.instructorName}</span>
+        </p>
+        <p className="font-medium text-base">{course.title}</p>
         <div>
-          <div>
-          </div>
-          {course.fee === 0 ? <p className='text-sm text-[#0BC01E]'>Free</p> : <p className='text-sm'><span>₦ {course.fee}</span> <span className='line-through	text-gray'>{course.strikedFee}</span></p>}
+          <div></div>
+          {course.fee === 0 ? (
+            <p className="text-sm text-[#0BC01E]">Free</p>
+          ) : (
+            <p className="text-sm">
+              <span>₦ {course.fee}</span>{" "}
+              <span className="line-through	text-gray">{course.strikedFee}</span>
+            </p>
+          )}
         </div>
-        <div className='flex'>
+        <div className="flex">
           <div>
-            <p className='text-xs my-1'>Students {course.enrolledStudents.length}</p>
-            <div className='flex ml-1'>
-              {course.enrolledStudents.slice(0, 6).map(course => <img key={course._id} src={course.profilePicture ? course.profilePicture : '/images/user.png'} className='w-5 rounded-full h-5 -ml-1' alt="" />)}
+            <p className="text-xs my-1">
+              Students {course.enrolledStudents.length}
+            </p>
+            <div className="flex ml-1">
+              {course.enrolledStudents.slice(0, 6).map((course) => (
+                <img
+                  key={course._id}
+                  src={
+                    course.profilePicture
+                      ? course.profilePicture
+                      : "/images/user.png"
+                  }
+                  className="w-5 rounded-full h-5 -ml-1"
+                  alt=""
+                />
+              ))}
             </div>
           </div>
           <div></div>
         </div>
-
       </div>
 
-      <div className='lg:w-52 my-auto'>
-        <button onClick={() => setOpen(true)} className='p-2 w-full my-1 bg-primary rounded-sm'>Enrol Now</button>
-        <button onClick={() => setAppointment(true)} className='p-2 w-full bg-primary rounded-sm my-1'>Enquire</button>
-        <button onClick={() => setShare(true)} className='p-2 w-full bg-primary rounded-sm my-1'>Share</button>
+      <div className="lg:w-52 my-auto">
+        <button
+          onClick={() => setOpen(true)}
+          className="p-2 w-full my-1 bg-primary rounded-sm"
+        >
+          Enrol Now
+        </button>
+        <button
+          onClick={() => setAppointment(true)}
+          className="p-2 w-full bg-primary rounded-sm my-1"
+        >
+          Enquire
+        </button>
+        <button
+          onClick={() => setShare(true)}
+          className="p-2 w-full bg-primary rounded-sm my-1"
+        >
+          Share
+        </button>
       </div>
-      <ShareModal open={share} course={course} handleClick={() => setShare(false)} />
-      <AppointmentModal open={appointment} handleClick={() => setAppointment(false)} to={course.instructorId} />
-      <CourseDetails course={course} open={open} action={"Course"} type='enroll' call={call} handleClick={() => setOpen(false)} />
+      <ShareModal
+        open={share}
+        course={course}
+        handleClick={() => setShare(false)}
+      />
+      <AppointmentModal
+        open={appointment}
+        handleClick={() => setAppointment(false)}
+        to={course.instructorId}
+      />
+      <CourseDetails
+        workspace={course}
+        course={course}
+        open={open}
+        action={"Course"}
+        type="enroll"
+        call={call}
+        handleClick={() => setOpen(false)}
+      />
     </div>
-
   );
 };
 
